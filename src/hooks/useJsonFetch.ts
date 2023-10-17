0743c8b71065166158fc8d314035f6afcafd74d2
Liaksej <tarusov.liaksej@gmail.com>
2023-10-17 13:44:42 +0200

    Add useJsonFetch custom hook for data fetching
    Created a new custom hook named useJsonFetch to simplify fetching data from APIs within functional components based on React. This hook uses 'useReducer' for state management internally to handle 'loading', 'data', and 'error' states during an API request. The hook takes an 'url' as a parameter to facilitate the reuse of the component for various API endpoints. Now, performing API request and handling its states become more manageable and neat in functional components using this hook.

---
+++ src/hooks/useJsonFetch.ts
@@ -0,0 +1,71 @@
+import { useEffect, useReducer } from "react";
+import { threadId } from "worker_threads";
+import { Simulate } from "react-dom/test-utils";
+import error = Simulate.error;
+
+interface State {
+  loading: boolean;
+  error: any;
+  data: any;
+}
+
+interface Actions {
+  type: "FETCH_INIT" | "FETCH_SUCCESS" | "FETCH_FAILURE";
+  payload?: {
+    data?: any;
+    error?: string | null;
+  };
+}
+
+function reducer(state: State, action: Actions) {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return {
+        loading: true,
+        error: null,
+        data: null,
+      };
+    case "FETCH_SUCCESS":
+      return {
+        loading: false,
+        error: null,
+        data: action.payload?.data,
+      };
+    case "FETCH_FAILURE":
+      return {
+        loading: false,
+        error: action.payload?.error,
+        data: null,
+      };
+    default:
+      return state;
+  }
+}
+
+export function useJsonFetch(url: string, opts?: any) {
+  const [state, dispatch] = useReducer(reducer, {
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_INIT" });
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(res.statusText);
+        }
+      })
+      .then((data) => {
+        dispatch({ type: "FETCH_SUCCESS", payload: { data: data } });
+      })
+      .catch((error) => {
+        dispatch({ type: "FETCH_FAILURE", payload: { error: error } });
+      });
+  }, [url]);
+
+  return [state.data, state.loading, state.error];
+}
